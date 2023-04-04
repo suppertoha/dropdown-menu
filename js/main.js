9e2650306fe98d4af119de54ae0f876b3b96685d
Toxa <suppertoha@yandex.ru>
2023-04-04 12:11:32 +0300

    ll

--- js/main.js
+++ js/main.js
@@ -2,6 +2,35 @@ window.addEventListener('load', function () {
 
 
   //! accordion
+
+  document.querySelectorAll(".accordion-start-js_first").forEach((el) => {
+    el.classList.remove("open"); // Удаляем класс "open" у всех элементов с классом "accordion-start-js"
+    
+    el.addEventListener("click", () => {
+      const content = el.nextElementSibling;
+      let nextAccordion = el.nextElementSibling;
+      
+      while (nextAccordion && !nextAccordion.classList.contains("accordion-start-js_first")) {
+        nextAccordion = nextAccordion.nextElementSibling;
+      }
+      
+      document.querySelectorAll(".accordion-start-js_first").forEach((accordion) => {
+        if (accordion !== el) {
+          accordion.classList.remove("open"); // Удаляем класс "open" у всех элементов, кроме текущего аккордеона
+          accordion.nextElementSibling.classList.remove("open"); // Удаляем класс "open" у всех элементов с классом "accordion-content-js", кроме текущего
+        }
+      });
+      
+      el.classList.toggle("open");
+      content.classList.toggle("open");
+      
+      if (nextAccordion && nextAccordion.classList.contains("accordion-content-js_first")) {
+        nextAccordion.classList.remove("open");
+      }
+    });
+  });
+
+
   document.querySelectorAll(".accordion-start-js").forEach((el) => {
     el.addEventListener("click", () => {
       const content = el.nextElementSibling;
@@ -20,22 +49,5 @@ window.addEventListener('load', function () {
     });
   });
 
-  
-
 });
 
-//document.querySelectorAll(".accordion-start-js").forEach((el) => {
-//  el.addEventListener("click", () => {
-//    const content = el.nextElementSibling;
-//    const accordions = document.querySelectorAll(".accordion-content-js");
-
-//    accordions.forEach((elem) => {
-//      if (elem !== content) { // закрыть все аккордеоны, кроме текущего
-//        elem.classList.remove("open");
-//      }
-//    });
-
-//    el.classList.toggle("open");
-//    content.classList.toggle("open"); // переключить класс open текущего аккордеона
-//  });
-//});
